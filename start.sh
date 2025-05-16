4295474cb8579af4fc28f0abfa21fd24248fd060
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2025-05-16 17:21:22 -0500

    15p

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # 1. Instala TensorFlow primero
+pip install --upgrade pip
 pip install transformers[tf]
 pip install tensorflow
+pip install transformers==4.40.1
+pip install tokenizers==0.19.1
 
 # 2. Luego instala el resto de los paquetes
 pip install -r requirements.txt
