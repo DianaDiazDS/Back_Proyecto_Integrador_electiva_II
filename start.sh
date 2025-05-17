1de9951335533d08d2dcff85406aa82384a9feae
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2025-05-16 21:12:44 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-# 1. Instala TensorFlow primero
-pip install --upgrade pip
-pip install transformers[tf]
-pip install tensorflow==2.15.0
-pip install transformers==4.35.0 
-pip install tokenizers==0.13.3
-
-# 2. Luego instala el resto de los paquetes
-pip install -r requirements.txt
-
-# 3. Inicia la app
-uvicorn main:app --host 0.0.0.0 --port $PORT
+# Solo arrancar la app, asumiendo que todo está instalado
+exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-8080}
