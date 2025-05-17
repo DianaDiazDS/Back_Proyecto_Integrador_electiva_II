47db759c415cfac3671b0442a2f87282b4338a9a
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2025-05-17 07:52:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -220,17 +220,17 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="backend-api")
 
+origins = [
+    "http://127.0.0.1:5500",
+    "https://glittery-platypus-06821f.netlify.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=[
-        "Content-Type",
-        "Authorization",
-        "Accept",
-        "X-Requested-With"
-    ],
-    expose_headers=["Content-Disposition"]
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
