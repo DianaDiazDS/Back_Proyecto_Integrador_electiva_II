d594025c4cb315ffd50f69d615a450602a9fe571
LuisPinto9 <luisenriquepinto9@gmail.com>
2025-05-17 12:54:19 -0500

    Arreglo

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Activar entorno si es necesario (descomenta si usas venv)
+# source venv/bin/activate
 export TF_ENABLE_ONEDNN_OPTS=0
 export TF_CPP_MIN_LOG_LEVEL=2
-exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-8080}
+# Ejecutar con gunicorn en modo producción
+exec gunicorn -w 4 -b 0.0.0.0:${PORT:-8080} main:app
